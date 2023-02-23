7d6e50520a1a666dc092bc2598889482b565ae73
samuelran <samuelbesserransby@gmail.com>
2023-02-23 18:32:54 +0100

    Added changes for 23/2 assignment

--- bird-api/app.js
+++ bird-api/app.js
@@ -20,4 +20,29 @@ app.get("/birds/:id",(req, res) => {
     }
 });
 
-app.listen(8080);
+app.post("/birds",(req,res) => {
+    const {name, species } =req.body;
+    const newBird = {
+        id:birds.length + 1,
+        name,
+        species
+    };
+    birds.push(newBird)
+    res.json(newBird)
+})
+
+app.delete("/birds/:id",(req, res) => {
+    const id = Number (req.params.id);
+    const birdIndex = birds.findIndex((bird) => bird.id === id);
+    if (birdIndex >= 0)  {
+        birds.splice(birdIndex, 1);
+        res.send("bird with id ${id} deleted");
+    } else {
+        res.status(404).send("Unable to find bird")
+    }
+    
+})
+
+app.listen(8080, () => {
+    console.log("Server is now running on port", 8080);
+});
